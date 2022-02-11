7787174ee189986695b110c2ade3e353ceb90641
Allen Lee <allen870619@gmail.com>
2022-02-11 15:14:19 +0800

    adjust keyboard utils

--- Sources/CommonUtils/Keyboard.swift
+++ Sources/CommonUtils/Keyboard.swift
@@ -8,184 +8,110 @@
 import Foundation
 import UIKit
 
-
-struct keyboardData{
-    static let offset :CGFloat = 16
-    static var moreOffset :CGFloat = 0
-    /* 暫存輸入框元件 */
-    static var currentUIView : UIView?
-    /* 暫存 View 的範圍 */
-    static var rect : CGRect?
-    static var viewController : UIViewController!
-    static var scrollView: UIScrollView?
+open class KBShifterViewController: UIViewController{
+    fileprivate var kbData = KeyboardData()
     
-    static func isDataNil() -> Bool{
-        return currentUIView == nil || rect == nil || viewController == nil
-    }
-}
-
-class KeyboardUtils{
-    init(viewController: UIViewController){
-        keyboardData.viewController = viewController
-        keyboardData.rect = viewController.view.frame
+    // set mode
+    public enum ShiftMode{
+        case absolute
+        case safeArea
+        case scrollView
     }
     
-    //targetTextField
-    func setTargetView(_ view: UIView?, moreOffset : CGFloat = 0) {
-        keyboardData.currentUIView = view
-        keyboardData.moreOffset = moreOffset
+    public var shiftMode: ShiftMode = .safeArea{
+        didSet{
+            unSubscribe()
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        }
     }
     
-    var targetView :UIView?{
+    // targetView
+    public var kbTargetView: UIView?{
+        set{
+            kbData.currentUIView = newValue
+        }
         get{
-            return keyboardData.currentUIView
+            kbData.currentUIView
+        }
+    }
+    
+    public func unSubscribe(){
+        kbData.currentUIView = nil
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    /* move up screen when showing keyboard */
+    @objc public func keyboardWillShow(note: NSNotification) {
+        if let currentView = kbData.currentUIView {
+            // keyboard info
+            let userInfo = note.userInfo!
+            let kbHeight = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.size.height
+            let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+            
+            // target view
+            let targetY = currentView.superview?.convert(currentView.frame, to: self.view).maxY ?? 0
+            let visibleRectWithoutKeyboard = ViewUtils.screenHeight - kbHeight
+            
+            // check whether has been covered
+            if targetY >= visibleRectWithoutKeyboard {
+                switch shiftMode{
+                case .safeArea:
+                    let shift :CGFloat = (targetY - visibleRectWithoutKeyboard) + kbData.offset + kbData.moreOffset
+                    UIView.animate(withDuration: duration){[self] in
+                        self.additionalSafeAreaInsets.bottom = shift
+                        self.view.layoutIfNeeded()
+                    }
+                case .scrollView:
+                    guard let scrollView = kbTargetView as? UIScrollView else{
+                        return
+                    }
+                    let navShift = self.navigationController?.navigationBar.frame.maxY ?? 0
+                    scrollView.contentInset.bottom = targetY - visibleRectWithoutKeyboard + kbData.offset + kbData.moreOffset + navShift
+                case .absolute: // not recommand
+                    var rect = self.view.frame
+                    /* 計算上移距離 */
+                    rect.origin.y -= (targetY - visibleRectWithoutKeyboard) + kbData.offset + kbData.moreOffset
+                    UIView.animate(withDuration: duration){
+                        self.view.frame = rect
+                    }
+                }
+            }
+        }
+    }
+    
+    /* move down screen when showing keyboard */
+    @objc public func keyboardWillHide(note: NSNotification) {
+        /* 鍵盤隱藏時將畫面下移回原樣 */
+        let keyboardAnimationDetail = note.userInfo as! [String: AnyObject]
+        let duration = TimeInterval(truncating: keyboardAnimationDetail[UIResponder.keyboardAnimationDurationUserInfoKey]! as! NSNumber)
+        UIView.animate(withDuration: duration){[self] in
+            switch shiftMode{
+            case .safeArea:
+                self.additionalSafeAreaInsets.bottom = 0
+                self.view.layoutIfNeeded()
+            case .scrollView:
+                guard let scrollView = kbTargetView as? UIScrollView else{
+                    return
+                }
+                scrollView.contentInset.bottom = .zero
+            case .absolute:
+                if self.view.frame.origin.y < 0{
+                    self.view.frame = self.view.frame.offsetBy(dx: 0, dy: -self.view.frame.origin.y)
+                }
+            }
+        }completion: { [self] _ in
+            if shiftMode != .scrollView{
+                kbData.currentUIView = nil
+            }
         }
     }
 }
 
-extension UIViewController{
-    /*
-     Set observer and target tf to use move up/down
-     Use BaseViewController to enable this feature
-     
-     keyboard = KeyboardUtils(viewController: self)
-     keyboard.setRect(rect: view.bounds)
-     
-     NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-     NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-     */
-    
-    /* move up screen when showing keyboard */
-    @objc func keyboardWillShow(note: NSNotification) {
-        if(keyboardData.isDataNil()){
-            return
-        }
-        
-        if let currentTextField = keyboardData.currentUIView {
-            let userInfo = note.userInfo!
-            /* 取得鍵盤尺寸 */
-            let keyboard = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.size
-            let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
-            /* 計算輸入框最底部Y座標，原Y座標為上方位置，需要加上高度 */
-            let targetY = currentTextField.frame.maxY
-            /* 計算扣除鍵盤高度後的可視高度 */
-            let visibleRectWithoutKeyboard = ViewUtils.screenHeight - keyboard.height
-            /* 如果輸入框Y座標在可視高度外，表示鍵盤已擋住輸入框 */
-            if targetY >= visibleRectWithoutKeyboard {
-                var rect = keyboardData.rect!
-                /* 計算上移距離 */
-                rect.origin.y -= (targetY - visibleRectWithoutKeyboard) + keyboardData.offset + keyboardData.moreOffset
-                UIView.animate(withDuration: duration){
-                    self.view.frame = rect
-                }
-            }
-        }
-    }
-    
-    /* move down screen when showing keyboard */
-    @objc func keyboardWillHide(note: NSNotification) {
-        /* 鍵盤隱藏時將畫面下移回原樣 */
-        if(keyboardData.isDataNil()){
-            return
-        }
-        let keyboardAnimationDetail = note.userInfo as! [String: AnyObject]
-        let duration = TimeInterval(truncating: keyboardAnimationDetail[UIResponder.keyboardAnimationDurationUserInfoKey]! as! NSNumber)
-        if self.view.frame.origin.y < 0{
-            UIView.animate(withDuration: duration){
-                self.view.frame = self.view.frame.offsetBy(dx: 0, dy: -self.view.frame.origin.y)
-            }completion: { _ in
-                keyboardData.currentUIView = nil
-            }
-        }
-    }
-    
-    /* move up screen when showing keyboard */
-    @objc func keyboardWillShowSafe(note: NSNotification) {
-        if(keyboardData.isDataNil()){
-            return
-        }
-        
-        if let currentTextField = keyboardData.currentUIView {
-            let userInfo = note.userInfo!
-            /* 取得鍵盤尺寸 */
-            let keyboard = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.size
-            let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
-            /* 計算輸入框最底部Y座標，原Y座標為上方位置，需要加上高度 */
-            let targetY = currentTextField.superview?.convert(currentTextField.frame, to: nil).maxY ?? 0
-            /* 計算扣除鍵盤高度後的可視高度 */
-            let visibleRectWithoutKeyboard = ViewUtils.screenHeight - keyboard.height
-            /* 如果輸入框Y座標在可視高度外，表示鍵盤已擋住輸入框 */
-            if targetY >= visibleRectWithoutKeyboard {
-                /* 計算上移距離 */
-                let shift :CGFloat = (targetY - visibleRectWithoutKeyboard) + keyboardData.offset + keyboardData.moreOffset
-                UIView.animate(withDuration: duration){
-                    self.additionalSafeAreaInsets.bottom = shift
-                    self.view.layoutIfNeeded()
-                }
-            }
-        }
-    }
-    
-    /* move down screen when showing keyboard */
-    @objc func keyboardWillHideSafe(note: NSNotification) {
-        /* 鍵盤隱藏時將畫面下移回原樣 */
-        if(keyboardData.isDataNil()){
-            return
-        }
-        let keyboardAnimationDetail = note.userInfo as! [String: AnyObject]
-        let duration = TimeInterval(truncating: keyboardAnimationDetail[UIResponder.keyboardAnimationDurationUserInfoKey]! as! NSNumber)
-        UIView.animate(withDuration: duration){
-            self.additionalSafeAreaInsets.bottom = 0
-            self.view.layoutIfNeeded()
-        }completion: { _ in
-            keyboardData.currentUIView = nil
-        }
-    }
-    
-    /* move up screen when showing keyboard */
-    @objc func keyboardWillShowScroll(note: NSNotification) {
-        if(keyboardData.isDataNil()){
-            return
-        }
-        
-        if let currentView = keyboardData.currentUIView {
-            let userInfo = note.userInfo!
-            /* 取得鍵盤尺寸 */
-            let keyboard = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.size
-            /* 計算輸入框最底部Y座標，原Y座標為上方位置，需要加上高度 */
-            let targetY = currentView.superview?.convert(currentView.frame, to: nil).maxY ?? 0
-            /* 計算扣除鍵盤高度後的可視高度 */
-            let visibleRectWithoutKeyboard = ViewUtils.screenHeight - keyboard.height
-            /* 如果輸入框Y座標在可視高度外，表示鍵盤已擋住輸入框 */
-            if targetY >= visibleRectWithoutKeyboard {
-                /* 計算上移距離 */
-                let viewOriY = currentView.superview?.convert(currentView.frame, to: keyboardData.scrollView).minY ?? 0
-                let navShift = self.navigationController?.navigationBar.frame.maxY ?? 0
-                let shift :CGFloat = (viewOriY - (visibleRectWithoutKeyboard - currentView.frame.height)) + keyboardData.offset + keyboardData.moreOffset + navShift
-                keyboardData.scrollView?.contentInset.bottom = keyboard.height
-                keyboardData.scrollView?.setContentOffset(CGPoint(x: 0, y: shift), animated: true)
-            }
-        }
-    }
-    
-    /* move down screen when showing keyboard */
-    @objc func keyboardWillHideScroll(note: NSNotification) {
-        /* 鍵盤隱藏時將畫面下移回原樣 */
-        if(keyboardData.isDataNil()){
-            return
-        }
-        let keyboardAnimationDetail = note.userInfo as! [String: AnyObject]
-        let duration = TimeInterval(truncating: keyboardAnimationDetail[UIResponder.keyboardAnimationDurationUserInfoKey]! as! NSNumber)
-        UIView.animate(withDuration: duration){
-            keyboardData.scrollView?.contentInset.bottom = .zero
-        }completion: { _ in
-            keyboardData.currentUIView = nil
-        }
-    }
-    
-    /* tap to hide keyboard */
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+public extension UIViewController{
+    func tapToHideKeyboard() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
@@ -193,4 +119,13 @@ extension UIViewController{
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
+}
+
+private struct KeyboardData{
+    // offest
+    let offset :CGFloat = 16
+    var moreOffset :CGFloat = 0
+    
+    // view
+    var currentUIView : UIView?
 }
