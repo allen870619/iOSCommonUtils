ada8139b8d1c12a7da78fcb9b95c25e5676a5660
Allen Lee <allen870619@gmail.com>
2022-03-08 09:56:26 +0800

    Adding Date Extension

--- Tests/iOSCommonUtilsTests/CommonUtilsTests.swift
+++ Tests/iOSCommonUtilsTests/CommonUtilsTests.swift
@@ -2,10 +2,10 @@ import XCTest
 @testable import iOSCommonUtils
 
 final class iOSCommonUtilsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(test(), "Hello, World!")
+    func testDateExtension(){
+        let date = Date()
+        XCTAssertEqual(date.year, 2022)
+        XCTAssertEqual(date.month, 3)
+        XCTAssertEqual(date.day, 8)
     }
 }
